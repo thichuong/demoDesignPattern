98de63357718864fcddba25ad87a22527135a01f
thichuong <18520539@gm.uit.edu.vn>
2022-11-15 22:36:16 +0700

    update

--- Facade/Facade.cs
+++ Facade/Facade.cs
@@ -24,34 +24,39 @@ namespace Facade
                 switch (drink)
                 {
                     case 1:
-                        order.addDrink(HighlandCoffeeFactory.getInstance().GetCoffee("Highland coffee", 150, 0, 10));
+                        order.addDrink(HighlandCoffeeFactory.getInstance().GetCoffee("Highland coffee", 150, 0, 10000));
                         break;
                         break;
                     case 2:
-                        order.addDrink(HighlandCoffeeFactory.getInstance().GetCoffee("Highland  milk coffee", 150, 50, 15));
+                        order.addDrink(HighlandCoffeeFactory.getInstance().GetCoffee("Highland  milk coffee", 150, 50, 15000));
                         break;
                     case 3:
-                        order.addDrink(TrungNguyenCoffeeFactory.getInstance().GetCoffee("Trung Nguyen coffee", 180, 0, 12));
+                        order.addDrink(TrungNguyenCoffeeFactory.getInstance().GetCoffee("Trung Nguyen coffee", 180, 0, 12000));
                         break;
                     case 4:
-                        order.addDrink(TrungNguyenCoffeeFactory.getInstance().GetCoffee("Trung Nguyen milk coffee", 180, 50, 16));
+                        order.addDrink(TrungNguyenCoffeeFactory.getInstance().GetCoffee("Trung Nguyen milk coffee", 180, 50, 16000));
                         break;
                     default:
-                        order.addDrink(TrungNguyenCoffeeFactory.getInstance().GetCoffee("Trung Nguyen milk coffee", 180, 50, 16));
+                        order.addDrink(TrungNguyenCoffeeFactory.getInstance().GetCoffee("Trung Nguyen milk coffee", 180, 50, 16000));
                         break;
                 }
             }
 
+            Console.WriteLine("You order: ");
+
             foreach (var item in order.Drinks)
             {
+                Console.Write("     ");
                 item.Print();
             }
+            Console.WriteLine();
+
         }
         public void Pay()
         {
             calc = new Calculator();
             int sum = calc.Sum(order);
-            Console.WriteLine("You paid " + sum);
+            Console.WriteLine("You paid " + sum + " VND");
         }
     }
 }
--- Facade/Program.cs
+++ Facade/Program.cs
@@ -9,7 +9,12 @@ namespace Facade
         static void Main(string[] args)
         {
             Facade facade = new Facade();
+
             facade.orderDrink(new List<int> { 1,2,2,3,4,1});
+            // 1 : Highland coffee
+            // 2 : Highland  milk coffee
+            // 3 : Trung Nguyen coffee
+            // 4 + .....: Trung Nguyen milk coffee
             facade.Pay();
 
             Console.ReadKey(true);
