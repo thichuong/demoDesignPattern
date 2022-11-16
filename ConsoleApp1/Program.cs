ec836a65293f64cd356aff89ef8b037d589dcc3b
thichuong <18520539@gm.uit.edu.vn>
2022-11-16 19:10:09 +0700

    Update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,184 +2,66 @@
 
 namespace RefactoringGuru.DesignPatterns.AbstractFactory.Conceptual
 {
-    // The Abstract Factory interface declares a set of methods that return
-    // different abstract products. These products are called a family and are
-    // related by a high-level theme or concept. Products of one family are
-    // usually able to collaborate among themselves. A family of products may
-    // have several variants, but the products of one variant are incompatible
-    // with products of another.
-   
-    public abstract class IAbstractFactory
+    public class Facade
     {
+        protected Subsystem1 _subsystem1;
 
-        public abstract IAbstractProductA CreateProductA();
+        protected Subsystem2 _subsystem2;
 
-        public abstract IAbstractProductB CreateProductB();
-       
-    }
-
-    // Concrete Factories produce a family of products that belong to a single
-    // variant. The factory guarantees that resulting products are compatible.
-    // Note that signatures of the Concrete Factory's methods return an abstract
-    // product, while inside the method a concrete product is instantiated.
-    class ConcreteFactory1 : IAbstractFactory
-    {
-        public override IAbstractProductA CreateProductA()
+        public Facade(Subsystem1 subsystem1, Subsystem2 subsystem2)
         {
-            return new ConcreteProductA1();
-        }        
-
-        public override IAbstractProductB CreateProductB()
-        {
-            return new ConcreteProductB1();
+            this._subsystem1 = subsystem1;
+            this._subsystem2 = subsystem2;
         }
 
-        private static ConcreteFactory1 instance = new ConcreteFactory1();
-
-        private ConcreteFactory1()
+        
+        public string Operation()
         {
-        }
-
-        public static ConcreteFactory1 getInstance()
-        {
-            if (instance == null)
-            {
-                instance = new ConcreteFactory1();
-            }
-
-            return instance;
+            string result = "Facade initializes subsystems:\n";
+            result += this._subsystem1.operation1();
+            result += this._subsystem2.operation1();
+            result += "Facade orders subsystems to perform the action:\n";
+            result += this._subsystem1.operationN();
+            result += this._subsystem2.operationZ();
+            return result;
         }
     }
 
-    // Each Concrete Factory has a corresponding product variant.
-    class ConcreteFactory2 : IAbstractFactory
+    // The Subsystem can accept requests either from the facade or client
+    // directly. In any case, to the Subsystem, the Facade is yet another
+    // client, and it's not a part of the Subsystem.
+    public class Subsystem1
     {
-        public override IAbstractProductA CreateProductA()
+        public string operation1()
         {
-            return new ConcreteProductA2();
+            return "Subsystem1: Ready!\n";
         }
 
-        public override IAbstractProductB CreateProductB()
+        public string operationN()
         {
-            return new ConcreteProductB2();
-        }
-
-        private static ConcreteFactory2 instance = new ConcreteFactory2();
-
-        private ConcreteFactory2()
-        {
-        }
-
-        public static ConcreteFactory2 getInstance()
-        {
-            if (instance == null)
-            {
-                instance = new ConcreteFactory2();
-            }
-
-            return instance;
+            return "Subsystem1: Go!\n";
         }
     }
 
-    // Each distinct product of a product family should have a base interface.
-    // All variants of the product must implement this interface.
-    public abstract class IAbstractProductA
+    public class Subsystem2
     {
-        public abstract string UsefulFunctionA() ;
-    }
-
-    // Concrete Products are created by corresponding Concrete Factories.
-    class ConcreteProductA1 : IAbstractProductA
-    {
-        public override string UsefulFunctionA()
+        public string operation1()
         {
-            return "The result of the product A1.";
+            return "Subsystem2: Get ready!\n";
+        }
+
+        public string operationZ()
+        {
+            return "Subsystem2: Fire!\n";
         }
     }
 
-    class ConcreteProductA2 : IAbstractProductA
-    {
-        public override string UsefulFunctionA()
-        {
-            return "The result of the product A2.";
-        }
-    }
 
-    // Here's the the base interface of another product. All products can
-    // interact with each other, but proper interaction is possible only between
-    // products of the same concrete variant.
-    public abstract class IAbstractProductB
-    {
-        // Product B is able to do its own thing...
-        public abstract string UsefulFunctionB();
-
-        // ...but it also can collaborate with the ProductA.
-        //
-        // The Abstract Factory makes sure that all products it creates are of
-        // the same variant and thus, compatible.
-        public abstract string AnotherUsefulFunctionB(IAbstractProductA collaborator) ;
-    }
-
-    // Concrete Products are created by corresponding Concrete Factories.
-    class ConcreteProductB1 : IAbstractProductB
-    {
-        public override string UsefulFunctionB()
-        {
-            return "The result of the product B1.";
-        }
-
-        // The variant, Product B1, is only able to work correctly with the
-        // variant, Product A1. Nevertheless, it accepts any instance of
-        // AbstractProductA as an argument.
-        public override string AnotherUsefulFunctionB(IAbstractProductA collaborator)
-        {
-            var result = collaborator.UsefulFunctionA();
-
-            return $"The result of the B1 collaborating with the ({result})";
-        }
-    }
-
-    class ConcreteProductB2 : IAbstractProductB
-    {
-        public override string UsefulFunctionB()
-        {
-            return "The result of the product B2.";
-        }
-
-        // The variant, Product B2, is only able to work correctly with the
-        // variant, Product A2. Nevertheless, it accepts any instance of
-        // AbstractProductA as an argument.
-        public override string AnotherUsefulFunctionB(IAbstractProductA collaborator)
-        {
-            var result = collaborator.UsefulFunctionA();
-
-            return $"The result of the B2 collaborating with the ({result})";
-        }
-    }
-
-    // The client code works with factories and products only through abstract
-    // types: AbstractFactory and AbstractProduct. This lets you pass any
-    // factory or product subclass to the client code without breaking it.
     class Client
     {
-        public void Main()
+        public static void ClientCode(Facade facade)
         {
-            // The client code can work with any concrete factory class.
-            Console.WriteLine("Client: Testing client code with the first factory type...");
-            ClientMethod(ConcreteFactory1.getInstance());
-            Console.WriteLine();
-
-            Console.WriteLine("Client: Testing the same client code with the second factory type...");
-            ClientMethod(ConcreteFactory1.getInstance());
-        }
-
-        public void ClientMethod(IAbstractFactory factory)
-        {
-            var productA = factory.CreateProductA();
-            var productB = factory.CreateProductB();
-
-            Console.WriteLine(productB.UsefulFunctionB());
-            Console.WriteLine(productB.AnotherUsefulFunctionB(productA));
+            Console.Write(facade.Operation());
         }
     }
 
@@ -187,7 +69,10 @@ namespace RefactoringGuru.DesignPatterns.AbstractFactory.Conceptual
     {
         static void Main(string[] args)
         {
-            new Client().Main();
+            Subsystem1 subsystem1 = new Subsystem1();
+            Subsystem2 subsystem2 = new Subsystem2();
+            Facade facade = new Facade(subsystem1, subsystem2);
+            Client.ClientCode(facade);
         }
     }
 }
